b15b0aa7facae73279eebf97964fa33b8f1c9ea9
Eric McConkie <mcconkiee@gmail.com>
2012-03-26 12:06:29 -0700

    adding MIT license

--- HintMakerExample/main.m
+++ HintMakerExample/main.m
@@ -3,7 +3,7 @@
 //  HintMakerExample
 //
 //  Created by Eric McConkie on 3/16/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
